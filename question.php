db787292972006cc7705489b953cf9f526d605b7
Caro03 <caroverstreken@hotmail.com>
2021-03-09 16:13:08 +0100

    Add question.php

---
+++ question.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meerkeuzevraag | peerhood</title>
+</head>
+<body>
+<div>
+        <input type="text" name="onderwerp" id="onderwerp" placeholder="Wat is het onderwerp?">
+    </div>
+    <div>
+        <input type="text" name="vraag" id="vraag" placeholder="Wat is de vraag?">
+    </div>
+    <div>
+        <input type="text" name="antwoord1" id="antwoord1" placeholder="Antwoord 1">
+    </div>
+    <div>
+        <input type="text" name="antwoord2" id="antwoord2" placeholder="Antwoord 2">
+    </div>
+    <div>
+        <input type="submit" value="Volgende stap">
+    </div>
+</body>
+</html>
