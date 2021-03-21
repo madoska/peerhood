580149d1fde1d7a3bc392db36cd0b5e72b86aaf7
Caro03 <caroverstreken@hotmail.com>
2021-03-21 19:12:19 +0100

    Changed Db class

--- classes/Db.php
+++ classes/Db.php
@@ -9,7 +9,7 @@ class Db {
     
         if(self::$conn === null) {
             try {
-                self::$conn = new PDO('mysql:host=' . SETTINGS['db']['host'] . ';port=' . SETTINGS['db']['port'] . ';dbname=' . SETTINGS['db']['dbname'], SETTINGS['db']['user'], SETTINGS['db']['password']);
+                self::$conn = new PDO('mysql:host=' . SETTINGS['db']['host'] . ';dbname=' . SETTINGS['db']['dbname'], SETTINGS['db']['user'], SETTINGS['db']['password']);
             } catch (PDOException $th) {
                 $error = $th->getMessage();
             }
