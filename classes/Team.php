d821e075391489d611b618768fabe60a3fdef05d
Medina Dadurgova <medina-dadurgova@hotmail.com>
2021-04-10 17:04:34 +0200

    view team and add students

--- classes/Team.php
+++ classes/Team.php
@@ -7,6 +7,7 @@ class Team
     private $teamID;
     private $courseID;
     private $teamname;
+    private $studentID;
 
     public function getTeamID()
     {
@@ -44,11 +45,43 @@ class Team
         return $this;
     }
 
+    public function getStudentID()
+    {
+        return $this->studentID;
+    }
+
+    public function setStudentID($studentID)
+    {
+        $this->studentID = $studentID;
+
+        return $this;
+    }
+
     public function fetchTeamsByCourse($courseID)
     {
         $conn = Db::connect();
         $statement = $conn->prepare("SELECT * FROM teams WHERE course_id = :courseID");
         $statement->bindParam(":courseID", $courseID);
+
+        $statement->execute();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    public function fetchTeamById($teamID){
+        $conn = Db::connect();
+        $statement = $conn->prepare("SELECT * FROM teams WHERE id = :teamID");
+        $statement->bindParam(":teamID", $teamID);
+
+        $statement->execute();
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    public function fetchMembersByTeamId($teamID){
+        $conn = Db::connect();
+        $statement = $conn->prepare("SELECT firstname, lastname FROM members INNER JOIN users ON members.student_id = users.id WHERE team_id = :teamID");
+        $statement->bindParam(":teamID", $teamID);
 
         $statement->execute();
         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -61,6 +94,24 @@ class Team
         $statement = $conn->prepare("INSERT INTO teams (course_id, teamname) VALUES(:courseID, :teamname)");
         $statement->bindParam(":courseID", $courseID);
         $statement->bindParam(":teamname", $teamname);
+        $result = $statement->execute();
+        return $result;
+    }
+
+    public function fetchStudentsByCourse($courseID){
+        $conn = Db::connect();
+        $statement = $conn->prepare("SELECT * FROM students INNER JOIN users ON students.student_id = users.id WHERE course_id = :courseID");
+        $statement->bindParam(":courseID", $courseID);
+        $statement->execute();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    public function addStudents($studentID, $teamID){
+        $conn = Db::connect();
+        $statement = $conn->prepare("INSERT INTO members (student_id, team_id) VALUES(:studentID, :teamID)");
+        $statement->bindParam(":studentID", $studentID);
+        $statement->bindParam(":teamID", $teamID);
         $result = $statement->execute();
         return $result;
     }
