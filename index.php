80c11189c7c50ad0239a058b2a1fcb6d3fabb9a0
Caro03 <caroverstreken@hotmail.com>
2021-04-13 19:55:58 +0200

    Added some classes for styling

--- index.php
+++ index.php
@@ -24,9 +24,12 @@ if ($role = 1) {
 </head>
 
 <body>
-    <h1 class="form_title text-xl md:text-2xl">Jouw cursussen</h1>
+    <h1 class="form_title text-2xl ml-10 mb-5 md:text-2xl">Jouw cursussen</h1>
+    <div class="bg-plus inline-block px-4 py-1 rounded-2xl">
+    <a href="addcourse.php"><img src="./images/+.png"></a>
+    </div>
     <?php foreach ($courses as $course) : ?>
-        <a href="course.php?id=<?php echo $course['id'] ?>" class="">
+        <a href="course.php?id=<?php echo $course['id'] ?>" class="ml-10 text-center form_btn py-2 w-64 md:w-72 h-12 shadow-md rounded-2xl text-white mb-2 block">
             <?php echo $course['coursename']; ?>
         </a>
         <br>
