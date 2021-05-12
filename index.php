b1dd9d38d4f1c97409f7f0b4a95e4ea7057c840f
Caro03 <caroverstreken@hotmail.com>
2021-05-12 12:23:38 +0200

    Added breakpoints & css to index

--- index.php
+++ index.php
@@ -25,11 +25,11 @@ if ($role = 1) {
 
 <body>
     <div class="px-5 py-5 mb-10 gradient rounded-b-xl">
-        <h2 class="ml-10 text-2xl text-white form_title">Een overzicht van jouw vakken</h2>
+        <h2 class="text-2xl text-center text-white form_title">Een overzicht van jouw vakken</h2>
     </div>
-    <div class="flex items-center mb-10">
-        <h2 class="w-2/3 mb-5 ml-10 text-2xl form_title md:text-2xl">Jouw cursussen</h2>
-        <div class="flex items-center justify-center w-1/3">
+    <div class="flex items-center justify-center mb-10">
+        <h2 class="mb-5 mr-10 text-2xl text-center form_title md:text-2xl">Jouw cursussen</h2>
+        <div class="flex items-center justify-center">
             <div class="w-12 h-12 text-center bg-plus rounded-2xl hover:opacity-90">
                 <a href="addcourse.php">
                     <span class="text-4xl font-bold text-white">+</span>
@@ -38,20 +38,15 @@ if ($role = 1) {
         </div>
     </div>
     <?php foreach ($courses as $course) : ?>
-        <a href="course.php?id=<?php echo $course['id'] ?>" style='background-color:<?php printf("#%06X\n", mt_rand(0, 0x222222)); ?>' class="block w-64 h-12 py-2 mb-2 ml-10 text-center text-white shadow-md hover:opacity-90 md:w-72 rounded-2xl">
+        <a class="block w-64 h-12 py-2 mb-2 ml-auto mr-auto text-center text-white shadow-md hover:opacity-90 md:w-72 rounded-2xl" href="course.php?id=<?php echo $course['id'] ?>" style='background-color:<?php printf("#%06X\n", mt_rand(0, 0x222222)); ?>'>
             <?php echo $course['coursename']; ?>
         </a>
         <br>
     <?php endforeach; ?>
 
-    <div class="flex flex-wrap">
-        <div class="w-full bg-white shadow-xl">
-            <a class="w-1/3" href="#"><img src="./images/trophy.png"></a>
-            <a class="w-1/3" href="index.php"><img src="./images/users.png"></a>
-            <a class="w-1/3" href="#"><img src="./images/profile.png"></a>
-        </div>
-    </div>
-
 </body>
+<footer>
+    <?php include_once('nav.inc.php'); ?>
+</footer>
 
 </html>
