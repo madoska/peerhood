ec322e1874240532bca8fea3708111b5e83691ff
Caro03 <caroverstreken@hotmail.com>
2021-05-12 12:41:15 +0200

    Added css to editteam

--- editteam.php
+++ editteam.php
@@ -16,7 +16,7 @@ if (isset($_GET['id'])) {
     $students = $fetchStudentsByCourse->fetchStudentsByCourse($courseID);
 }
 
-if(!empty($_POST['addstudent'])){
+if (!empty($_POST['addstudent'])) {
     $studentID = $_POST['student'];
     $addStudents = new Team();
     $addStudents->setStudentID($studentID);
@@ -29,20 +29,33 @@ if(!empty($_POST['addstudent'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="build/tailwind.css">
     <title>PEERHOOD | Studenten toevoegen</title>
 </head>
+
 <body>
-    <h1>Voeg studenten toe</h1>
+    <div class="px-5 py-5 mb-10 gradient rounded-b-xl">
+        <h1 class="text-3xl text-center text-white form_title">Dag</h1>
+    </div>
+
+    <h1 class="mb-10 text-2xl text-center form_title md:text-2xl">Voeg studenten toe</h1>
     <form action="" method="post">
-        <?php foreach($students as $student): ?>
+        <?php foreach ($students as $student) : ?>
             <input type="checkbox" id="student" name="student" value="<?php echo $student['student_id'] ?>">
-            <label for="student"><?php echo $student['firstname'] . " " . $student['lastname']?></label><br>
+            <label for="student"><?php echo $student['firstname'] . " " . $student['lastname'] ?></label><br>
         <?php endforeach ?>
-        <input type="submit" value="Voeg studenten toe" name="addstudent">
+        <input class="block h-12 mb-2 ml-auto mr-auto text-center text-white shadow-md w-52 sm:w-64 form_btn md:w-72 rounded-2xl" type="submit" value="Voeg studenten toe" name="addstudent">
     </form>
 </body>
+
+<footer>
+    <?php include_once('nav.inc.php'); ?>
+</footer>
+
 </html>
