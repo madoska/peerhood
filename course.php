78e381c598044e8ea395872290e35b94c8ba7d70
Caro03 <caroverstreken@hotmail.com>
2021-05-12 12:22:55 +0200

    Added css to course

--- course.php
+++ course.php
@@ -28,12 +28,16 @@ if (isset($_GET['id'])) {
 
 <body>
     <div class="px-5 py-5 mb-10 gradient rounded-b-xl">
-        <h1 class="ml-10 text-3xl text-white form_title">Dag</h1>
+        <h1 class="text-3xl text-center text-white form_title">Dag</h1>
     </div>
 
-    <h1 class="w-2/3 ml-10 text-2xl mb-14 form_title md:text-2xl"><?php echo $course['coursename'] ?></h1>
-    <a class="block w-64 h-12 py-2 mb-2 mb-5 ml-10 text-center shadow-md hover:opacity-90 dark_text bg-secondary-button md:w-72 rounded-2xl" href="question.php">Bekijk quizzen</a>
-    <a class="block w-64 h-12 py-2 mb-2 ml-10 text-center shadow-md hover:opacity-90 dark_text bg-secondary-button md:w-72 rounded-2xl" href="teams.php?id=<?php echo $course['id'] ?>">Bekijk teams</a>
+    <h1 class="text-2xl text-center mb-14 form_title md:text-2xl"><?php echo $course['coursename'] ?></h1>
+    <a class="block w-64 h-12 py-2 mb-2 mb-5 ml-auto mr-auto text-center shadow-md hover:opacity-90 dark_text bg-secondary-button md:w-72 rounded-2xl" href="question.php">Bekijk quizzen</a>
+    <a class="block w-64 h-12 py-2 mb-2 ml-auto mr-auto text-center shadow-md hover:opacity-90 dark_text bg-secondary-button md:w-72 rounded-2xl" href="teams.php?id=<?php echo $course['id'] ?>">Bekijk teams</a>
 </body>
+
+<footer>
+    <?php include_once('nav.inc.php'); ?>
+</footer>
 
 </html>
