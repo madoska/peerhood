87f580b6b4a7285c106463103e59a3b5724ad554
Caro03 <caroverstreken@hotmail.com>
2021-06-14 17:45:57 +0200

    Nav is responsive

--- nav.inc.php
+++ nav.inc.php
@@ -1,5 +1,7 @@
-<div class="fixed bottom-0 flex flex-wrap justify-center w-11/12 py-5 mb-10 space-x-10 overflow-hidden bg-white rounded-full shadow-md sm:space-x-16">
-    <a href="#"><img src="./images/trophy.png"></a>
-    <a href="index.php"><img src="./images/users.png"></a>
-    <a href="#"><img src="./images/profile.png"></a>
+<div class="flex justify-center">
+    <div class="fixed bottom-0 flex flex-wrap justify-center py-5 mb-10 ml-auto mr-auto space-x-10 bg-white rounded-full shadow-md w-60 sm:w-80 md:w-96 sm:space-x-16">
+        <a href="#"><img src="./images/trophy.png"></a>
+        <a href="index.php"><img src="./images/users.png"></a>
+        <a href="profile.php"><img src="./images/profile.png"></a>
+    </div>
 </div>
