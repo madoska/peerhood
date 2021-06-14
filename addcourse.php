7c9c11e83b8eaa03c480ca7e30094a8f7f86fdef
Miguel Lamm <49272909+MiguelLamm@users.noreply.github.com>
2021-06-14 17:27:08 +0200

    quick hotfix, sorryyy

--- addcourse.php
+++ addcourse.php
@@ -2,7 +2,7 @@
 include_once(__DIR__ . "/inc/session.inc.php");
 include_once(__DIR__ . "/classes/Course.php");
 
-if (isset($_POST['submit'])) {
+if (isset($_POST['submit']) && $role == 1) {
     if (!empty($_POST['coursename'])) {
         $coursename = $_POST['coursename'];
         $code = substr(md5(uniqid(mt_rand(), true)), 0, 6);
@@ -21,6 +21,11 @@ if (isset($_POST['submit'])) {
         echo "geen cursusnaam";
     }
 }
+
+if (isset($_POST['controleer'])) {
+   
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -36,17 +41,30 @@ if (isset($_POST['submit'])) {
 
 <body>
     <div class="px-5 py-5 mb-10 gradient rounded-b-xl">
+      
+    <?php if ($role == 1) { ?>
+
         <h2 class="text-2xl text-center text-white form_title">Welk vak geef jij?</h2>
     </div>
     <h2 class="text-2xl text-center mb-14 form_title md:text-2xl">Nieuwe cursus</h2>
-    <?php if ($role == 1) { ?>
+
         <form class="form" action="" method="post">
             <input class="block w-64 mb-2 ml-auto mr-auto bg-transparent border-b border-black form_field md:w-72" type="text" name="coursename" placeholder="Naam van jouw vak">
             <br>
             <input class="block w-64 h-12 mb-2 ml-auto mr-auto text-white shadow-md form_btn md:w-72 rounded-2xl" type="submit" value="Creeër cursus" name="submit">
         </form>
     <?php } else { ?>
-        <p>Alleen docenten kunnen cursussen aanmaken!</p>
+
+        <h2 class="text-2xl text-center text-white form_title">Welk vak volg jij?</h2>
+    </div>
+    <h2 class="text-2xl text-center mb-14 form_title md:text-2xl">Nieuwe cursus</h2>
+    <form class="form" action="" method="post">
+        <input class="block w-64 mb-2 ml-auto mr-auto bg-transparent border-b border-black form_field md:w-72" type="text" name="CheckName" placeholder="Naam van het vak">
+            <br>
+        <input class="block w-64 mb-2 ml-auto mr-auto bg-transparent border-b border-black form_field md:w-72" type="text" name="CheckCode" placeholder="Code">
+            <br>
+            <input class="block w-64 h-12 mb-2 ml-auto mr-auto text-white shadow-md form_btn md:w-72 rounded-2xl" type="submit" value="Controleer" name="controleer">
+            </form>
     <?php } ?>
     <a href="index.php">terug</a>
 </body>
